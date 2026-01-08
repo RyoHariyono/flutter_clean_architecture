ed494041ce749b4473f3b8c972ae597f89d77575
lordbruh0 <ryohariyono17@gmail.com>
2026-01-08 10:21:36 +0700

    feat: :alien: make request to api using retrofit
    create a api request using retrofit to get all article data

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/core/constants/constants.dart';
+import 'package:flutter_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:retrofit/http.dart';
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsApiBaseUrl)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<Map<String, dynamic>> getNewsArticles({
+    @Query("apiKey") String? apiKey,
+    @Query("country") String? country,
+    @Query("category") String? category,
+  });
+}
